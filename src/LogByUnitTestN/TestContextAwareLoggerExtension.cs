56e34222dfb7ae21ea718146110ef553aecc15c3
Neil Fitzgerald <nfitzgerald@cimpress.com>
2017-04-20 10:21:06 -0400

    Initial commit - untested.

---
+++ src/LogByUnitTestN/TestContextAwareLoggerExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using NLog;
+using NUnit.Framework;
+
+namespace LogByUnitTestN
+{
+    public static class TestContextAwareLoggerExtension
+    {
+        public static Logger GetLogger(this object obj)
+        {
+            try
+            {
+                var testName = TestContext.CurrentContext.Test.Name;
+                foreach (char someChar in Path.GetInvalidFileNameChars())
+                {
+                    testName = testName.Replace(someChar, '_');
+                }
+                return LogManager.GetLogger(testName);
+            }
+            catch (NullReferenceException)
+            {
+                return LogManager.GetLogger(obj.GetType().Name);
+            }
+        }
+    }
+}
